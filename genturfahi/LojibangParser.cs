325bd8510d847b3233e03800d153027738f8ab75
baban <babanba.n@gmail.com>
2015-11-22 04:16:33 +0900

    first commit

---
+++ genturfahi/LojibangParser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace genturfahi
+{
+    class LojibangParser : INotifyPropertyChanged
+    {
+        string lojibanContent = "";
+        string parseResult = "";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Create the OnPropertyChanged method to raise the event
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public string LojibanContent {
+            get { return lojibanContent; }
+            set {
+                lojibanContent = value;
+
+                OnPropertyChanged("lojibanContent");
+            }
+        }
+
+        public string ParseResult
+        {
+            get { return parseResult; }
+            set
+            {
+                parseResult = value;
+
+                OnPropertyChanged("parseResult");
+            }
+        }
+
+        public void parse() { 
+        }
+    }
+}
